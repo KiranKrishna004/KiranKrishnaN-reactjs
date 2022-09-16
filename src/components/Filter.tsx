de1875d937551ab75b9d14ac82d9957d88cfee5a
KiranKrishna004 <kiran.004narath@gmail.com>
2022-09-17 00:07:36 +0530

    feat: implement product details page

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -12,6 +12,16 @@ export const Filter = ({
     <div className='flex w-full flex-col items-center'>
       <div className='font-semibold pb-2'>Filter by Categories</div>
       <div className=' flex flex-row pb-7'>
+        <button
+          onClick={() => setFilter("")}
+          className={
+            filter !== ""
+              ? "bg-transparent text-blue-700 font-semibold py-1 px-2 border border-blue-500 rounded mx-2"
+              : "bg-blue-500 text-white font-semibold py-1 px-2 border border-white rounded mx-2"
+          }
+        >
+          All
+        </button>
         {categories.map((categories: Categories, index: number) => (
           <button
             key={index}
@@ -25,12 +35,6 @@ export const Filter = ({
             {categories.name}
           </button>
         ))}
-        <button
-          onClick={() => setFilter("")}
-          className='bg-red-400 text-white border border-white rounded mx-2 h-1/2 py-1 px-1 self-end text-xs'
-        >
-          Reset
-        </button>
       </div>
     </div>
   );
